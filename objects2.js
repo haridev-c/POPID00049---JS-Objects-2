eaf8871a6c45cffafed3d42cffb3495f4136a5db
Haridev <haridev.8281@outlook.com>
2024-02-28 00:03:28 +0530

    First Commit

---
+++ objects2.js
@@ -0,0 +1,14 @@
+let user1 = {
+    upvotes: 30,
+    downvotes: 15
+}
+
+let user2 = {
+    upvotes: 50,
+    downvotes: 29
+}
+let finalvote1 = user1.upvotes - user1.downvotes;
+let finalvote2 = user2.upvotes - user2.downvotes;
+
+console.log(finalvote1);
+console.log(finalvote2);
